a44889cea75674b24277a36dd5086ee12a4c1116
Can Noah <87754848+cannoahgkt@users.noreply.github.com>
2025-03-04 11:15:45 +0100

    New

---
+++ v0-user-next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'portfolio'
+
+const userConfig = {
+  output: 'export',
+  assetPrefix: isProd ? `/portfolio` : '',
+  basePath: isProd ? `/portfolio` : '',
+  trailingSlash: true,
+}
+
+export default userConfig
